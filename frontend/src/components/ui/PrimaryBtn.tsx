c78a692176714da11176dc1e4a6636fc12a1711a
Faruk Spahic <farukspahictz@gmail.com>
2025-06-19 16:52:56 +0200

    Added custom input and primary btn

---
+++ frontend/src/components/ui/PrimaryBtn.tsx
@@ -0,0 +1,21 @@
+import type { ButtonHTMLAttributes, ReactNode } from "react";
+import { cn } from "../../lib/utils";
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  icon?: ReactNode;
+}
+
+export function PrimaryBtn({ icon, className, children, ...props }: Props) {
+  return (
+    <button
+      className={cn(
+        "bg-indigo-600 hover:bg-indigo-500 duration-200 text-white rounded-full px-7 py-3 flex space-x-4 cursor-pointer",
+        className
+      )}
+      {...props}
+    >
+      {icon}
+      <span>{children}</span>
+    </button>
+  );
+}
---
+++ frontend/src/components/ui/PrimaryInput.tsx
@@ -0,0 +1,27 @@
+import type { InputHTMLAttributes, ReactNode } from "react";
+import { cn } from "../../lib/utils";
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  icon?: ReactNode;
+  className?: string;
+  wrapperClassName?: string;
+}
+
+export function PrimaryInput({
+  icon,
+  className,
+  wrapperClassName,
+  ...props
+}: Props) {
+  return (
+    <div
+      className={cn(
+        "flex space-x-4 items-center border rounded-xl px-4 focus-within:outline focus-within:outline-2 focus-within:outline-indigo-500",
+        wrapperClassName
+      )}
+    >
+      {icon}
+      <input className={cn("outline-none w-full py-2", className)} {...props} />
+    </div>
+  );
+}
