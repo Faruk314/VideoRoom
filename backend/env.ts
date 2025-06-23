392f3ebc4d73c27bbd5cadcf0ef6d47785e3f8d6
Faruk Spahic <farukspahictz@gmail.com>
2025-06-23 19:52:34 +0200

    Added db env variables

--- backend/env.ts
+++ backend/env.ts
@@ -9,6 +9,12 @@ export const env = createEnv({
     FRONTEND_URL: z.string().min(1),
     BACKEND_PORT: z.string().min(1).transform(Number),
 
+    DB_PASSWORD: z.string().min(1),
+    DB_USER: z.string().min(1),
+    DB_NAME: z.string().min(1),
+    DB_HOST: z.string().min(1),
+    DB_PORT: z.string().transform(Number),
+
     REDIS_HOST: z.string().min(1),
     REDIS_PORT: z.string().min(1).transform(Number),
     REDIS_PASSWORD: z.string().min(1),
