5494030bee66957d4d9e95cfda7a8c9c1006c78b
Faruk Spahic <farukspahictz@gmail.com>
2025-06-24 16:59:21 +0200

    Added login form and page

---
+++ frontend/src/features/auth/components/LoginForm.tsx
@@ -0,0 +1,76 @@
+import { LoginSchema } from "../schemas/auth";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { PrimaryBtn } from "../../../components/buttons/PrimaryBtn";
+import { PrimaryInput } from "../../../components/buttons/PrimaryInput";
+import type { LoginInput } from "../../types/auth";
+import { useLoginMutation } from "../queries/auth";
+import { Lock, LogIn, Mail } from "lucide-react";
+import Separator from "../../../components/Separator";
+import { useNavigate } from "react-router-dom";
+import { Spinner } from "../../../components/loaders/Spinner";
+
+export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginInput>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const { mutate: loginUser, isPending } = useLoginMutation();
+  const navigate = useNavigate();
+
+  async function onSubmit(data: LoginInput) {
+    loginUser(data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="grid gap-8">
+      <div className="grid gap-4">
+        <PrimaryInput
+          placeholder="Enter you email"
+          icon={<Mail />}
+          {...register("email")}
+          type="text"
+        />
+        {errors.email && <p className="text-red-600">{errors.email.message}</p>}
+
+        <PrimaryInput
+          placeholder="Enter your password"
+          icon={<Lock />}
+          {...register("password")}
+          type="password"
+        />
+
+        {errors.password && (
+          <p className="text-red-600">{errors.password.message}</p>
+        )}
+      </div>
+
+      <PrimaryBtn
+        icon={<LogIn />}
+        className="w-full"
+        disabled={isPending}
+        type="submit"
+      >
+        {isPending ? <Spinner className="h-6 w-6" /> : "Sign In"}
+      </PrimaryBtn>
+
+      <Separator />
+
+      <PrimaryBtn
+        onClick={() => navigate("/register")}
+        type="button"
+        className="w-full"
+      >
+        Create Account
+      </PrimaryBtn>
+    </form>
+  );
+}
---
+++ frontend/src/features/auth/pages/Login.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "../components/LoginForm";
+
+export default function Login() {
+  return (
+    <section className="flex items-center justify-center w-full h-[100vh]">
+      <LoginForm />
+    </section>
+  );
+}
