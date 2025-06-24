4ab6591fc4a523272a08f35ee6f497d5a229954b
Faruk Spahic <farukspahictz@gmail.com>
2025-06-24 18:25:02 +0200

    Added loader in app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,26 @@ import { Routes, Route } from "react-router-dom";
 import { CreateChannel } from "./features/channel/pages/CreateChannel";
 import { Channel } from "./features/channel/pages/Channel";
 import { ToastContainer } from "react-toastify";
+import Login from "./features/auth/pages/Login";
+import Register from "./features/auth/pages/Register";
+import { useLoginStatusQuery } from "./features/auth/queries/auth";
+import Loader from "./components/loaders/Loader";
 
 function App() {
+  const { isPending } = useLoginStatusQuery();
+
+  if (isPending) {
+    return <Loader />;
+  }
+
   return (
     <>
       <Routes>
-        <Route path="/" element={<CreateChannel />} />
-        <Route path="/room/:id" element={<Channel />} />
+        <Route path="/home" element={<CreateChannel />} />
+        <Route path="/channel/:id" element={<Channel />} />
+
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
       <ToastContainer />
     </>
