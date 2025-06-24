150d6cc461b978b763ec8cd3eddf0e69acbaffe2
Faruk Spahic <farukspahictz@gmail.com>
2025-06-24 13:27:14 +0200

    Installed and configured react query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,18 @@ import "./index.css";
 import App from "./App.tsx";
 import { SocketProvider } from "./providers/socket.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <SocketProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </SocketProvider>
+    <QueryClientProvider client={queryClient}>
+      <SocketProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </SocketProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
