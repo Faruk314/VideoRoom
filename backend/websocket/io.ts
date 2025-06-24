67df9da2dcb5909ac7e35462d096873e055e2108
Faruk Spahic <farukspahictz@gmail.com>
2025-06-24 19:04:39 +0200

    Added socket io middleware

--- backend/websocket/io.ts
+++ backend/websocket/io.ts
@@ -1,5 +1,7 @@
 import { Server as ServerIO, Socket } from "socket.io";
 import { env } from "../env";
+import ChannelListeners from "./listeners/channels/channel";
+import { getUserSessionById } from "redis/methods/session";
 
 function createSocketServer(httpServer: import("http").Server) {
   const io = new ServerIO(httpServer, {
@@ -10,8 +12,34 @@ function createSocketServer(httpServer: import("http").Server) {
     },
   });
 
+  io.use(async (socket: Socket, next) => {
+    const cookieHeader = socket.request.headers.cookie;
+
+    if (!cookieHeader) return next(new Error("Missing cookie header"));
+
+    const cookies = Object.fromEntries(
+      cookieHeader.split("; ").map((c) => c.split("="))
+    );
+
+    const sessionId = cookies.sessionId;
+
+    if (!sessionId) return next(new Error("Missing session cookie"));
+
+    const session = await getUserSessionById(sessionId);
+
+    if (!session) {
+      return next(new Error("Invalid session"));
+    }
+
+    socket.userId = session.userId;
+
+    next();
+  });
+
   io.on("connection", (socket: Socket) => {
-    console.log(`user connected with id ${socket.id}`);
+    console.log(`user connected with id ${socket.userId}`);
+
+    new ChannelListeners(io, socket).registerListeners();
   });
 }
 
