a7eb0ad740704eac5bbc8a6a1728a2c88a4ebf26
Faruk Spahic <farukspahictz@gmail.com>
2025-06-24 19:04:59 +0200

    Modified socket provider

--- frontend/src/providers/socket.tsx
+++ frontend/src/providers/socket.tsx
@@ -2,12 +2,18 @@ import { useEffect, useState } from "react";
 import type { ReactNode } from "react";
 import { io as clientIO, type Socket } from "socket.io-client";
 import { SocketContext } from "../context/socket";
+import { useUserStore } from "../features/user/store/user";
 
 export function SocketProvider({ children }: { children: ReactNode }) {
   const [socket, setSocket] = useState<Socket | null>(null);
   const [isConnected, setIsConnected] = useState(false);
+  const { isLogged } = useUserStore();
 
   useEffect(() => {
+    if (isLogged && socket) return;
+
+    if (!isLogged) return;
+
     const WS_URL = import.meta.env.VITE_WEBSOCKET_URL;
     const SOCKET_PATH = "/ws";
 
@@ -28,7 +34,7 @@ export function SocketProvider({ children }: { children: ReactNode }) {
     return () => {
       socketInstance.disconnect();
     };
-  }, []);
+  }, [isLogged]);
 
   return (
     <SocketContext.Provider value={{ socket, isConnected }}>
