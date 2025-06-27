c79ed424e33363ff44aac8d1a24d814cb9e4ba8f
Faruk Spahic <farukspahictz@gmail.com>
2025-06-27 19:10:00 +0200

    Sorted participants so local participants goes first

--- frontend/src/features/channel/queries/channel.ts
+++ frontend/src/features/channel/queries/channel.ts
@@ -2,18 +2,31 @@ import { useQuery } from "@tanstack/react-query";
 import { getErrorMessage } from "../../../lib/error";
 import { getChannel } from "../api/channel";
 import { useChannelStore } from "../store/channel";
+import { useUserStore } from "../../user/store/user";
+import type { IParticipant } from "../types/channel";
 
 export function useChannelQuery(channelId: string) {
   const { setParticipants } = useChannelStore();
+  const { user: localUser } = useUserStore();
 
   return useQuery({
     queryKey: ["channel", channelId],
     enabled: !!channelId,
     queryFn: async () => {
       try {
-        const data = await getChannel(channelId);
+        const data: { participants: IParticipant[] } = await getChannel(
+          channelId
+        );
 
-        setParticipants(data.participants);
+        const sortedParticipants: IParticipant[] = Array.from(
+          data.participants.values()
+        ).sort((a, b) => {
+          if (a.user.userId === localUser?.userId) return -1;
+          if (b.user.userId === localUser?.userId) return 1;
+          return 0;
+        });
+
+        setParticipants(sortedParticipants);
 
         return data;
       } catch (error) {
