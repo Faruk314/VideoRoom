6ffbe3a5c52f5da821a5b07b17654eec259ddb2f
Faruk Spahic <farukspahictz@gmail.com>
2025-07-02 18:04:01 +0200

    Added methods to get media stream, audio stream and display stream

--- frontend/src/features/media/hooks/useMedia.tsx
+++ frontend/src/features/media/hooks/useMedia.tsx
@@ -43,5 +43,47 @@ export function useMedia() {
     setSelectedCamera(cameras[0]);
   }
 
-  return { getAudioDevices, getVideoDevices };
+  async function getMediaStream() {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    const audioTrack = stream.getAudioTracks()[0];
+    const videoTrack = stream.getVideoTracks()[0];
+
+    if (!audioTrack || !videoTrack)
+      throw new Error("Missing audio or video track");
+
+    return { stream, audioTrack, videoTrack };
+  }
+
+  async function getAudioStream() {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const audioTrack = stream.getAudioTracks()[0];
+
+    if (!audioTrack) throw new Error("No audio track found");
+
+    return { stream, audioTrack };
+  }
+
+  async function getDisplayStream() {
+    const stream = await navigator.mediaDevices.getDisplayMedia({
+      video: true,
+      audio: true,
+    });
+
+    const screenTrack = stream.getVideoTracks()[0];
+
+    if (!screenTrack) throw new Error("No screen track found");
+
+    return { stream, screenTrack };
+  }
+
+  return {
+    getAudioDevices,
+    getVideoDevices,
+    getMediaStream,
+    getAudioStream,
+    getDisplayStream,
+  };
 }
