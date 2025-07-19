a341a2b936d3e3a2c8fe2cfb0e70fef9a3be0495
Faruk Spahic <farukspahictz@gmail.com>
2025-07-19 18:10:47 +0200

    Show permission modal in micBtn when audio access is not granted

--- frontend/src/components/buttons/MicrophoneBtn.tsx
+++ frontend/src/components/buttons/MicrophoneBtn.tsx
@@ -1,6 +1,8 @@
 import { Mic, MicOff } from "lucide-react";
 import { IconBtn } from "./IconBtn";
 import useChannelManager from "../../features/channel/hooks/useChannelManager";
+import { useMediaStore } from "../../features/media/store/media";
+import MediaPermissions from "../modals/MediaPermissions";
 
 interface Props {
   micMuted: boolean;
@@ -8,12 +10,19 @@ interface Props {
 
 export default function MicrophoneBtn({ micMuted }: Props) {
   const { toogleMicrophone } = useChannelManager();
+  const { hasAudioPermission } = useMediaStore();
 
   return (
-    <IconBtn
-      onClick={toogleMicrophone}
-      description={micMuted ? "Unmute" : "Mute"}
-      icon={micMuted ? <MicOff /> : <Mic />}
-    />
+    <>
+      {hasAudioPermission ? (
+        <IconBtn
+          onClick={toogleMicrophone}
+          description={micMuted ? "Unmute" : "Mute"}
+          icon={micMuted ? <MicOff /> : <Mic />}
+        />
+      ) : (
+        <MediaPermissions type="audio" />
+      )}
+    </>
   );
 }
