ee46b66ddb7ec279549b41e58ca4ee576c4eec3e
Faruk Spahic <farukspahictz@gmail.com>
2025-07-20 16:17:15 +0200

    Added camera availability check

--- frontend/src/components/buttons/CameraBtn.tsx
+++ frontend/src/components/buttons/CameraBtn.tsx
@@ -1,8 +1,11 @@
+import { useEffect, useState } from "react";
 import useChannelManager from "../../features/channel/hooks/useChannelManager";
 import { useMediaStore } from "../../features/media/store/media";
 import MediaPermissions from "../modals/MediaPermissions";
+import MissingMediaDevice from "../modals/MissingMediaDevice";
 import { IconBtn } from "./IconBtn";
 import { Video, VideoOff } from "lucide-react";
+import { useMedia } from "../../features/media/hooks/useMedia";
 
 interface Props {
   camMuted: boolean;
@@ -11,18 +14,26 @@ interface Props {
 export default function CameraBtn({ camMuted = false }: Props) {
   const { toogleCamera } = useChannelManager();
   const { hasVideoPermission } = useMediaStore();
+  const { hasAvailableDevice } = useMedia();
+  const [hasCamDevice, setHasCamDevice] = useState(true);
+
+  useEffect(() => {
+    hasAvailableDevice("video")
+      .then(setHasCamDevice)
+      .catch(() => {
+        setHasCamDevice(false);
+      });
+  }, []);
+
+  if (!hasCamDevice) return <MissingMediaDevice type="camera" />;
+
+  if (!hasVideoPermission) return <MediaPermissions type="video" />;
 
   return (
-    <>
-      {hasVideoPermission ? (
-        <IconBtn
-          onClick={toogleCamera}
-          description={camMuted ? "Turn On Camera" : "Turn Off Camera"}
-          icon={camMuted ? <VideoOff /> : <Video />}
-        />
-      ) : (
-        <MediaPermissions type="video" />
-      )}
-    </>
+    <IconBtn
+      onClick={toogleCamera}
+      description={camMuted ? "Turn On Camera" : "Turn Off Camera"}
+      icon={camMuted ? <VideoOff /> : <Video />}
+    />
   );
 }
