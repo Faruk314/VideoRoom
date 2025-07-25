8774f95280f744c8f03a5ea9422b8c4c8371d881
Faruk Spahic <farukspahictz@gmail.com>
2025-07-25 20:53:30 +0200

    Changed positon of FullscreenBtn  on sm screen

--- frontend/src/features/channel/components/ChannelFooter.tsx
+++ frontend/src/features/channel/components/ChannelFooter.tsx
@@ -18,35 +18,46 @@ export default function ChannelFooter() {
   const { leaveChannel } = useChannel();
 
   return (
-    <div
-      className={classNames(
-        "fixed bottom-0 w-full mb-4 slide-up z-50",
-        isHovering ? "slide-up" : "slide-down"
-      )}
-    >
-      <div className="relative flex items-center justify-between w-full mx-auto py-2 px-4">
-        <div className="fixed bottom-0 left-1/2 -translate-x-1/2 flex items-center space-x-2 border border-gray-300 rounded-md px-2 py-2 shadow-md bg-white z-50">
-          <CameraBtn camMuted={localParticipant?.camMuted || false} />
-          <MicrophoneBtn micMuted={localParticipant?.micMuted || false} />
-          <ScreenShareBtn
-            isStreaming={localParticipant?.isStreaming || false}
-          />
-          <SettingsModal />
-          <IconBtn
-            onClick={leaveChannel}
-            description="Leave Call"
-            icon={<Phone size={20} />}
-            className="text-white bg-red-500 hover:bg-red-400"
-          />
-        </div>
+    <>
+      <div
+        className={classNames(
+          "fixed bottom-0 w-full mb-4 slide-up z-50",
+          isHovering ? "slide-up" : "slide-down"
+        )}
+      >
+        <div className="relative flex items-center justify-between w-full mx-auto py-2 px-4">
+          <div className="fixed bottom-0 left-1/2 -translate-x-1/2 flex items-center space-x-2 border border-gray-300 rounded-md px-2 py-2 shadow-md bg-white z-50">
+            <CameraBtn camMuted={localParticipant?.camMuted || false} />
+            <MicrophoneBtn micMuted={localParticipant?.micMuted || false} />
+            <ScreenShareBtn
+              isStreaming={localParticipant?.isStreaming || false}
+            />
+            <SettingsModal />
+            <IconBtn
+              onClick={leaveChannel}
+              description="Leave Call"
+              icon={<Phone size={20} />}
+              className="text-white bg-red-500 hover:bg-red-400"
+            />
+          </div>
 
-        <div className="fixed bottom-0 right-6 flex z-50">
-          <ChannelDetails />
-          <FullScreenBtn />
-        </div>
+          <div className="hidden md:flex fixed bottom-0 right-6 z-50">
+            <ChannelDetails />
+            <FullScreenBtn />
+          </div>
 
-        <HideParticipantsBtn />
+          <HideParticipantsBtn />
+        </div>
       </div>
-    </div>
+      <div
+        className={classNames(
+          "fixed top-2 left-2 z-50 md:hidden",
+          isHovering ? "flex" : "hidden"
+        )}
+      >
+        <ChannelDetails />
+        <FullScreenBtn />
+      </div>
+    </>
   );
 }
