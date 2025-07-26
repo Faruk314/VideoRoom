33d8e1a85a572a95720f3288610df235558878d7
Faruk Spahic <farukspahictz@gmail.com>
2025-07-26 19:56:43 +0200

    Added responsivnes to PrimaryBtn and Home page

--- frontend/src/features/channel/pages/CreateChannel.tsx
+++ frontend/src/features/channel/pages/CreateChannel.tsx
@@ -11,11 +11,9 @@ export function CreateChannel() {
   return (
     <section className="flex flex-col items-center justify-center w-full h-[100vh]">
       <div className="fixed top-0 p-4 flex items-center justify-between w-full">
-        <div className="flex items-baseline space-x-2 text-2xl md:text-3xl">
-          <span className="font-semibold">Welcome</span>
-          <span className="text-gray-500 text-xl md:text-2xl">
-            {user?.userName}
-          </span>
+        <div className="flex items-baseline space-x-2 text-xl md:text-3xl">
+          <span className="font-semibold text-2xl md:text-3xl">Welcome</span>
+          <span className="text-gray-500 md:text-2xl">{user?.userName}</span>
         </div>
 
         <PrimaryBtn onClick={() => logoutUser()} icon={<LogOut size={20} />}>
@@ -24,10 +22,10 @@ export function CreateChannel() {
       </div>
       <div className="flex flex-col justify-center items-center space-y-10">
         <div className="flex flex-col text-center px-4 md:px-0 items-center space-y-2">
-          <h1 className="text-4xl font-semibold">
+          <h1 className="text-2xl md:text-4xl font-semibold">
             Video Calls and Meetings for anyone!
           </h1>
-          <p className="text-gray-400 text-xl font-medium">
+          <p className="text-gray-400 md:text-xl font-medium">
             Enter your meeting code or create a new one to get started.
           </p>
         </div>
