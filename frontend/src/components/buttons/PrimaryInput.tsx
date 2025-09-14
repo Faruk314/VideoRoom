3489e820decdfc6a1bf3dcc55383a73dfaa8d786
Faruk Spahic <farukspahictz@gmail.com>
2025-09-14 14:22:10 +0200

    Changed PrimaryInput sm screen padding

--- frontend/src/components/buttons/PrimaryInput.tsx
+++ frontend/src/components/buttons/PrimaryInput.tsx
@@ -19,7 +19,7 @@ export function PrimaryInput({
     <div className="relative">
       <div
         className={cn(
-          "flex space-x-4 items-center border rounded-md px-4 py-2 md:py-3 focus-within:outline focus-within:outline-1",
+          "flex space-x-4 items-center border rounded-md px-4 py-[10px] md:py-3 focus-within:outline focus-within:outline-1",
           error
             ? "border-red-600 focus-within:outline-red-600"
             : "border-black focus-within:outline-indigo-500",
