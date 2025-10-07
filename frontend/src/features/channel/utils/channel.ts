0c62ea6df10f2391e5ea79bfdb79dff902b6bac7
Faruk Spahic <farukspahictz@gmail.com>
2025-10-07 18:53:57 +0200

    Added utility functions for extracting participant video and audio streams

--- frontend/src/features/channel/utils/channel.ts
+++ frontend/src/features/channel/utils/channel.ts
@@ -1,3 +1,5 @@
+import type { ILocalParticipant, IParticipant } from "../types/channel";
+
 function extractChannelId(value: string) {
   const channelUrlRegex =
     /^https?:\/\/localhost(?::5173)?\/channel\/([a-zA-Z0-9_-]{8})\/?$/;
@@ -11,4 +13,37 @@ function extractChannelId(value: string) {
   return null;
 }
 
-export { extractChannelId };
+function isLocalParticipant(
+  p: ILocalParticipant | IParticipant
+): p is ILocalParticipant {
+  return (p as ILocalParticipant).streams !== undefined;
+}
+
+function getVideoStream(
+  p: ILocalParticipant | IParticipant,
+  isDisplayStream?: boolean
+): MediaStream | undefined {
+  if (isLocalParticipant(p)) {
+    if (isDisplayStream) return p.streams?.screen;
+
+    if (p.camMuted) return undefined;
+
+    return p.streams?.video;
+  } else {
+    const track = isDisplayStream
+      ? p.consumers?.screen?.track
+      : p.consumers?.video?.track;
+
+    if (!isDisplayStream && p.camMuted) return undefined;
+
+    return track ? new MediaStream([track]) : undefined;
+  }
+}
+
+function getAudioStream(p: ILocalParticipant | IParticipant) {
+  if (isLocalParticipant(p)) return p.streams?.audio;
+  const track = p.consumers?.audio?.track;
+  return track ? new MediaStream([track]) : undefined;
+}
+
+export { extractChannelId, isLocalParticipant, getVideoStream, getAudioStream };
