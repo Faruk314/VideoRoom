3959ddc4a5766a125e1a65354919467d4fab262f
Faruk Spahic <farukspahictz@gmail.com>
2025-10-24 14:43:18 +0200

    Added utility function for formating message time

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { format, isToday } from "date-fns";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
+
+function formatMessageTime(date: Date, short = false): string {
+  if (short) {
+    return format(date, "h:mm a");
+  }
+
+  if (isToday(date)) {
+    return format(date, "h:mm a");
+  }
+
+  return format(date, "MMMM d 'at' h:mm a");
+}
+
+export { cn, formatMessageTime };
